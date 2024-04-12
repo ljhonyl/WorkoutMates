cd0194722c62f0a56fa9337bd682e1c05e2f4393
Jhony <jonathan.tr998@gmail.com>
2024-04-12 18:09:45 +0200

    Initial commit

---
+++ app/src/main/java/es/tiernoparla/dam/proyecto/workoutmates/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.tiernoparla.dam.proyecto.workoutmates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
